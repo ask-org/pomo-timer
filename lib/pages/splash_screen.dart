e27734fe60938f23b69ed1da7254b2fbf4fd5051
GosuCode <gosucode1945@gmail.com>
2023-12-31 17:17:06 +0545

    splash screen animation

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -8,7 +8,16 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 2),
+    vsync: this,
+  )..repeat(reverse: true);
+
+  late final Animation<double> _animation =
+      CurvedAnimation(parent: _controller, curve: Curves.easeIn);
+
   @override
   void initState() {
     super.initState();
@@ -16,33 +25,34 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   _navigateToHome() async {
-    await Future.delayed(Duration(milliseconds: 2000), () {});
+    await Future.delayed(const Duration(seconds: 2), () {});
     Navigator.pushReplacement(
         context, MaterialPageRoute(builder: (context) => const TimerPage()));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: screenHeight * 0.42,
-              width: screenWidth * 0.9,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(50),
-                child: Image.asset(
-                  "assets/images/time_untill.png",
-                  fit: BoxFit.fill,
-                ),
-              ),
+    return FadeTransition(
+      opacity: _animation,
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: SizedBox(
+            height: screenHeight * 0.42,
+            width: screenWidth * 0.9,
+            child: Image.asset(
+              "assets/images/time_untill.png",
+              fit: BoxFit.fill,
             ),
-          ],
+          ),
         ),
       ),
     );
