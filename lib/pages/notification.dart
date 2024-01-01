3ebed459d811a40984d7ed779a9c2f8cba2ac8bd
GosuCode <gosucode1945@gmail.com>
2024-01-01 18:10:24 +0545

    notification

---
+++ lib/pages/notification.dart
@@ -0,0 +1,59 @@
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
+import 'package:flutter_background_service_android/flutter_background_service_android.dart';
+
+Future<void> initializeService() async {
+  final service = FlutterBackgroundService();
+  await service.configure(
+      iosConfiguration: IosConfiguration(
+        autoStart: true,
+        onForeground: onStart,
+        onBackground: onIosBackground,
+      ),
+      androidConfiguration: AndroidConfiguration(
+          onStart: onStart, isForegroundMode: true, autoStart: true));
+}
+
+@pragma('vm:entry-point')
+onStart(ServiceInstance service) async {
+  DartPluginRegistrant.ensureInitialized();
+  if (service is AndroidServiceInstance) {
+    service.on('setAsForeground').listen((event) {
+      service.setAsForegroundService();
+    });
+    service.on('setAsForeground').listen((event) {
+      service.setAsBackgroundService();
+    });
+  }
+  service.on('stop service').listen((event) {
+    service.stopSelf();
+  });
+
+  int totalSeconds = 0;
+
+  String formatTime(int seconds) {
+    int minutes = seconds ~/ 60;
+    int remainingSeconds = seconds % 60;
+    return '$minutes:${remainingSeconds.toString().padLeft(2, "0")}';
+  }
+
+  Timer.periodic(const Duration(seconds: 1), (timer) async {
+    if (service is AndroidServiceInstance) {
+      if (await service.isForegroundService()) {
+        service.setForegroundNotificationInfo(
+            title: "Time Untill", content: formatTime(totalSeconds));
+      }
+    }
+    service.invoke('update');
+  });
+}
+
+@pragma('vm:entry-point')
+Future<bool> onIosBackground(ServiceInstance serviceInstance) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  DartPluginRegistrant.ensureInitialized();
+  return true;
+}
