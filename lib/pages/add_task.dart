b416e87a98dd4a779777a71a785e99b40c7ca680
GosuCode <gosucode1945@gmail.com>
2024-02-05 09:21:10 +0545

    merge

--- lib/pages/add_task.dart
+++ lib/pages/add_task.dart
@@ -101,7 +101,7 @@ class _AddTaskState extends State<AddTask> {
                                               var tasks = box.getAt(index);
                                               return Dismissible(
                                                 background: Container(
-                                                  color: Color.fromARGB(255, 158, 142, 140),
+                                                  color: Color.fromARGB(255, 176, 75, 61),
                                                 ),
                                                 key: ValueKey(index),
                                                 onDismissed: (DismissDirection
